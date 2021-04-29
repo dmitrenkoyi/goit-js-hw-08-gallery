2a83f5e2f151717e75d4523a74c86c768c4401b0
Yulia-working <artemovatest@gmail.com>
2021-04-29 21:44:27 +0300

    add code  gallery.js

---
+++ gallery.js
@@ -0,0 +1,97 @@
+import galleryItems from './gallery-items.js';
+
+const galleryEl = document.querySelector('.js-gallery');
+
+const createGalleryMarkup = galleryItems.map(({ preview, original,  description}) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link"
+        href="${original}"
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+        alt="${description}"
+        />
+      </a>
+    </li>`
+    ;
+}).join(' ');
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+
+const modalEl = document.querySelector('.js-lightbox');
+const modalImg = document.querySelector('.lightbox__image');
+const closeBtn = document.querySelector('button[data-action="close-lightbox"]');
+const overlay = document.querySelector('.lightbox__overlay');
+
+galleryEl.addEventListener('click', onGallaryClick);
+modalEl.addEventListener('click', CloseModal);
+
+function onGallaryClick(e) {
+    e.preventDefault();
+    
+    const target = e.target;
+    if (target.nodeName !== 'IMG') {
+            return;
+        }
+   
+    openModal();   
+    
+    modalImg.src = target.dataset.source;
+    
+    closeBtn.addEventListener('click', CloseModal);
+    window.addEventListener('click', event => {
+        if (event.code === overlay) {
+            CloseModal();
+        }
+    });
+    
+    window.addEventListener('keydown', event => {
+      if (event.code === 'Escape') {
+        CloseModal();
+      }
+    });
+};
+
+// пролистывание клавишами "влево" и "вправо"
+
+const arrayImages = document.querySelectorAll('.gallery__image');
+const imagesSrc = [];
+
+arrayImages.forEach(e => {
+  imagesSrc.push(e.getAttribute('data-source'));
+});
+
+
+document.addEventListener('keydown', e => {
+  let newIndex = imagesSrc.indexOf(modalImg.src);
+  
+    if (newIndex < 0) {
+    return;
+  }
+  if (e.code === 'ArrowLeft') {
+    newIndex -= 1;
+    if (newIndex === -1) {
+      newIndex = imagesSrc.length - 1;
+    }
+  } else if (e.code === 'ArrowRight') {
+    newIndex += 1;
+    if (newIndex === imagesSrc.length) {
+      newIndex = 0;
+    }
+  }
+  modalImg.src = imagesSrc[newIndex];
+});
+
+
+function openModal() {
+    modalEl.classList.add('is-open');
+};
+
+function CloseModal(){
+    modalEl.classList.remove('is-open');
+    modalImg.src = '';
+};
